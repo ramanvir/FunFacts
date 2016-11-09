eb36933bfc3058b35f25b5afdf08358731f3f5cb
Ramanvir Sodhi <raman345@gmail.com>
2016-11-09 20:56:19 +0530

    - Quotes data added - A press on button will change the quote displayed.

---
+++ FunFacts/FactModel.swift
@@ -0,0 +1,14 @@
+//
+//  FactModel.swift
+//  FunFacts
+//
+//  Created by Ramanvir Sodhi on 09/11/16.
+//  Copyright © 2016 StarMediaLabs. All rights reserved.
+//
+
+struct FactModel {
+
+    let facts = ["My name is Raman", "I am going to become a gret iOS developer", "I'll become a very successful person"]
+
+}
+    
--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var funFactsLabel: UILabel!
+    
+    let factModel = FactModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        funFactsLabel.text = factModel.facts[0]
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showFunFact() {
+        
+        funFactsLabel.text = factModel.facts[1]
+        
+    }
 
 }
 
